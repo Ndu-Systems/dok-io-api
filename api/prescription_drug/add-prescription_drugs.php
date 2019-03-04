afe902dd55d97d2aa6b1ae07c499a89797fb5655
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-03-04 21:26:41 +0200

    paging css

---
+++ api/prescription_drug/add-prescription_drugs.php
@@ -0,0 +1,56 @@
+<?php
+include_once '../../config/Database.php';
+include_once '../../models/Prescription_Drug.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+$FirstName  = $data->FirstName;
+$Surname  = $data->Surname;
+$IdNumber  = $data->IdNumber;
+$DOB  = $data->DOB;
+$Gender  = $data->Gender;
+$Email  = $data->Email;
+$Cellphone  = $data->Cellphone;
+$AddressLine1  = $data->AddressLine1;
+$City  = $data->City;
+$Province  = $data->Province;
+$PostCode  = $data->PostCode;
+$CreateUserId  = $data->CreateUserId;
+$ModifyUserId  = $data->CreateUserId;
+$StatusId  = $data->StatusId;
+
+
+
+//connect to db
+$database = new Database();
+$db = $database->connect();
+
+//Instantiate user object
+
+$user = new Prescription_Drug($db);
+
+$result = $user->add(
+    $FirstName,
+    $Surname,
+    $IdNumber,
+    $DOB,
+    $Gender,
+    $Email,
+    $Cellphone,
+    $AddressLine1,
+    $City,
+    $Province,
+    $PostCode,
+    $CreateUserId,
+    $ModifyUserId,
+    $StatusId
+);
+
+echo json_encode($result);
+
+
+
+
+
+
+
