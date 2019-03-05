5ee78e96f88f993fb42e61e85a2a2baa31267d91
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-03-05 20:21:19 +0200

    get active meds

--- models/Medication.php
+++ models/Medication.php
@@ -1,33 +1,34 @@
 <?php
 
-class Medication {
+class Medication
+{
 
-    //DB stuff
-    private $conn;
+  //DB stuff
+  private $conn;
 
-    //return properties
-    public $medicationId;
-    public $name;
-    public $description;
-    public $unit;
-    public $dosage;
-    public $createdate;
-    public $CreateUserId;
-    public $ModifyDate;
-    public $ModifyUserId;
-    public $StatusId;
+  //return properties
+  public $medicationId;
+  public $name;
+  public $description;
+  public $unit;
+  public $dosage;
+  public $createdate;
+  public $CreateUserId;
+  public $ModifyDate;
+  public $ModifyUserId;
+  public $StatusId;
 
 
-    //Constructor required for Dependency Injection
-    public function __construct($db)
-    {
-      $this->conn = $db;
-    }
+  //Constructor required for Dependency Injection
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
 
-    //Get medication that were prescribed for a prescription
-    public function GetMedicationForPrescription($prescriptionId)
-    {
-       $query = "
+  //Get medication that were prescribed for a prescription
+  public function GetMedicationForPrescription($prescriptionId)
+  {
+    $query = "
         SELECT med.medicationId, med.name, med.description, pmd.unit, pmd.dosage
         FROM medication med
         LEFT JOIN prescription_medication_drug pmd ON med.medicationId = pmd.medicationId
@@ -35,12 +36,25 @@ class Medication {
         ORDER BY med.createdate 
        ";
 
-       //prepare query statement
-       $stmt = $this->conn->prepare($query);
-       
-       //Execute query
-       $stmt->execute(Array($prescriptionId));
-       $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-       return $result;
-    }
+    //prepare query statement
+    $stmt = $this->conn->prepare($query);
+
+    //Execute query
+    $stmt->execute(array($prescriptionId));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+  }
+  public function getMedications()
+  {
+    $query = "Select * from medication where StatusId = ?";
+
+    //prepare query statement
+    $stmt = $this->conn->prepare($query);
+
+    //Execute query
+    $stmt->execute(array(1));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+  }
 }
+
